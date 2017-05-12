f351c7c9d76cab3578c720ec121c5791424b99e8
jpgonzalez <jpgonzalez@paradigmadigital.com>
2017-05-12 15:54:11 +0200

    Rename flight

--- routedaas/src/main/java/com/paradigma/entities/Plane.java
+++ routedaas/src/main/java/com/paradigma/entities/Plane.java
@@ -1,8 +1,5 @@
 package com.paradigma.entities;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,19 +9,15 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Route {
-
+public class Plane {
 	
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long routeId;
+	private long planeId;
+	
+	private int seats;
+	private String maker;
+	private String model;
+		
 
-	private String origin;
-	private String destination;
-    private  LocalDateTime inaugurationDate;
-    private Plane plane; 
-    private List<String> flightsCode;
-	
-	
 }
